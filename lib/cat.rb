9e84de829ad09ee59670dec81bd4aa9f860213a4
tatiana.mojica <tatiana.mojica@bustle.com>
2018-12-06 03:49:13 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,12 @@
 class Cat
-  # code goes here
+  
+  attr_reader :name 
+  #name cannot be changed 
+  attr_accessor :mood
+  
+  def initialize(name, mood = "nervous")
+    @name = name 
+    @mood = mood  
+    #mood is changeable 
+  end 
 end
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,49 @@
 class Owner
-  # code goes here
+  attr_reader :species
+  attr_accessor :pets, :name, :fish, :cat, :dog
+  
+  @@all = []
+  
+  def initialize(species)
+    @pets = {fishes: [], cats: [], dogs: []}
+    @species = species 
+    @@all << self 
+  end 
+  
+  def self.count 
+    @@all.length 
+  end 
+  
+  def self.all 
+    @@all 
+  end 
+  
+  def self.reset_all 
+    @@all.clear
+  end 
+
+  def buy_cat(name) 
+    # making new instance initializing it by name 
+    # associate that new pet instance to the owner by adding it to the appropriate array-value
+    @pets[:cats] << Cat.new(name)
+    # OR 
+    # cat = Cat.new(name)
+    # @@pets[:cats] << cats  
+  end 
+  
+  def buy_dog(name)
+    @pets[:dogs] << Dog.new(name)
+  end
+  
+  def buy_fish(name)
+    @pets[:fishes] << Fish.new(name)
+  end 
+  
+  def say_species  
+    "I am a #{species}."
+  end 
+  
+  def walk_dogs
+    @pets[:dogs].collect {|mood| = "happy"
+  end 
 end
