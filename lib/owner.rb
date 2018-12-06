37f902766aca7938730c44cb8938f275bc4e036a
tatiana.mojica <tatiana.mojica@bustle.com>
2018-12-06 03:21:55 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,21 @@
 class Owner
-  # code goes here
+  
+  attr_accessor :pets, :fish, :cat, :dog
+  
+  @@pets = {fishes: [], cats: [], dogs: []}
+  @@all = []
+  
+  def initialize(species)
+    
+  end 
+  
+  
+  def self.all 
+    @@all 
+  end 
+  
+  def self.reset_all 
+    @@all.clear
+  end 
+  
 end
